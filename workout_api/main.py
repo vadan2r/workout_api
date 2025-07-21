2e29ee64bea17741fe6b426f2b8d46d3e5fa8c6e
vadan2r <vadan.2r@gmail.com>
2025-07-21 20:42:31 -0300

    Projeto DIO Criando API com fastapi, python e docker

--- workout_api/main.py
+++ workout_api/main.py
@@ -3,3 +3,7 @@ from workout_api.routers import api_router
 
 app = FastAPI(title='WorkoutApi')
 app.include_router(api_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, log_level="info", reload=True)
